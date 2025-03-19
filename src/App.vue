b2efd986214e1aad6b615c599c2e8757df30c60b
iAryanK <aryankinmail@gmail.com>
2025-03-19 21:39:17 +0530

    create: home route

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Navbar from '@/components/Navbar.vue'
+import Hero from './components/Hero.vue'
+import HomeCards from './components/HomeCards.vue'
+import JobListings from './components/JobListings.vue'
+</script>
 
 <template>
   <header>
-    <h1>Hello Vue !</h1>
+    <Navbar />
+    <Hero title="Become a Vue Dev" subtitle="Find the Vue job that fits your skills and needs" />
+    <HomeCards />
+    <JobListings showButton />
   </header>
 </template>
