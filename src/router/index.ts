b2e236c925f7c2b4e13ebe2cf422f81adf803f74
iAryanK <aryankinmail@gmail.com>
2025-03-19 19:13:52 +0530

    remove: irrelevant boilerplate

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+  routes: [],
 })
 
 export default router
